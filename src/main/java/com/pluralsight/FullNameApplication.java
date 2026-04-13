e613dba80ee6502c012c84aa78deeca7c04a845c
bryan <bryanmendez21@icloud.com>
2026-04-13 12:59:21 -0400

    added if statements so users name will be outputted based on what they provided.

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -9,18 +9,31 @@ public class FullNameApplication {
         System.out.println("What is your name: ");
         String firstName = dataCatcher.nextLine();
 
-        System.out.println("what's your suffix? if none write none: ");
-        String suffix = dataCatcher.nextLine();
+        System.out.println("what's your middle name? if none write none: ");
+        String middleName = dataCatcher.nextLine();
 
         System.out.println("What is your last name: ");
         String lastName = dataCatcher.nextLine();
 
-        if (suffix.equalsIgnoreCase("none")){
-            String fullName = firstName + lastName;
+        System.out.println("what's your suffix? if none write none: ");
+        String suffix = dataCatcher.nextLine();
+
+
+        if (suffix.equalsIgnoreCase("none") && (middleName.equalsIgnoreCase("none"))){
+            String fullName = firstName + " "+ lastName;
 
             System.out.println(fullName);
 
         }
+        else if (suffix.equalsIgnoreCase("none")){
+            String fullName = firstName + " " + middleName + " " + lastName;
+            System.out.println(fullName);
+        }
+        else{
+            String fullName = firstName + " " + middleName + " " + lastName + ","+ suffix;
+            System.out.println(fullName);
+        }
+
 
 
     }
