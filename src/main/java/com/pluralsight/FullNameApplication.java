b28094c0a30fa836e06a492fb8c5d70f4d4ae0b4
bryan <bryanmendez21@icloud.com>
2026-04-13 15:46:35 -0400

    add another another else if incase no middle suffix made it so that user doesnt have to type none

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -9,31 +9,34 @@ public class FullNameApplication {
         System.out.println("What is your name: ");
         String firstName = dataCatcher.nextLine();
 
-        System.out.println("what's your middle name? if none write none: ");
+        System.out.println("what's your middle name: ");
         String middleName = dataCatcher.nextLine();
 
         System.out.println("What is your last name: ");
         String lastName = dataCatcher.nextLine();
 
-        System.out.println("what's your suffix? if none write none: ");
+        System.out.println("what's your suffix: ");
         String suffix = dataCatcher.nextLine();
 
 
-        if (suffix.equalsIgnoreCase("none") && (middleName.equalsIgnoreCase("none"))){
+        if (suffix.equalsIgnoreCase("") && (middleName.equalsIgnoreCase(""))){
             String fullName = firstName + " "+ lastName;
 
             System.out.println(fullName);
 
         }
-        else if (suffix.equalsIgnoreCase("none")){
+        else if (suffix.equalsIgnoreCase("")){
             String fullName = firstName + " " + middleName + " " + lastName;
+            System.out.println(fullName);
+        }
+        else if (suffix.equalsIgnoreCase("")){
+            String fullName = firstName  + " " + lastName + "," + suffix ;
             System.out.println(fullName);
         }
         else{
             String fullName = firstName + " " + middleName + " " + lastName + ","+ suffix;
             System.out.println(fullName);
         }
-
 
 
     }
